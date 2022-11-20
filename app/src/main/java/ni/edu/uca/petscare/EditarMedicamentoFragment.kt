c1057858d39ead7794229828cba4a1b427f78ede
KevinP26 <kperezobando16@gmail.com>
2022-11-19 20:02:13 -0600

    Actualización Date

--- app/src/main/java/ni/edu/uca/petscare/EditarMedicamentoFragment.kt
+++ app/src/main/java/ni/edu/uca/petscare/EditarMedicamentoFragment.kt
@@ -59,7 +59,21 @@ class EditarMedicamentoFragment : Fragment() {
         datePicker.show(parentFragmentManager, "datePicker" )
     }
     fun onDateSelected(day: Int, month: Int, year: Int){
-        fbinding.etFechaFinEdit.setText("$year-$month-0$day")
+        if (day >= 1 && day <= 9 && month > 9) {
+            fbinding.etFechaFinEdit.setText("$year-$month-0$day")
+
+        }
+
+        if (day >= 1 && day <= 9 && month >= 1 && month <= 9) {
+            fbinding.etFechaFinEdit.setText("$year-0$month-0$day")
+
+        }
+        if (month >= 1 && month <= 9 && day >= 10) {
+            fbinding.etFechaFinEdit.setText("$year-0$month-$day")
+        }
+        if (month > 9 && day > 9) {
+            fbinding.etFechaFinEdit.setText("$year-$month-$day")
+        }
     }
 
     companion object {
